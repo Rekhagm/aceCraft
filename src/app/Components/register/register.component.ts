9177a70ab1c1f76539e4ec53bd8ef885a7e599cd
Rekhagm <rekha@cgvakindia.com>
2022-04-11 20:53:44 +0530

    commit

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -40,7 +40,7 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      Studentfirstname: ['', Validators.required, Validators.pattern('^[a-zA-Z \-\']+')],
+      Studentfirstname: ['', Validators.required],
       studentlastname: ['', Validators.required],
       email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.minLength(6)]],
